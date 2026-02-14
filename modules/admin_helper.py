ca653d4f2e1764fa5440785af2eb962ca4976a36
Rouletabille <rouletabille@mountyhall.com>
2026-02-14 09:34:04 +0100

    autovacuum fait le job

--- modules/admin_helper.py
+++ modules/admin_helper.py
@@ -8,6 +8,7 @@ from classes.user import User
 from classes.user_mh_call import MhCall
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+from sqlalchemy import sql
 import datetime, os, time, re
 import modules.globals as sg
 
@@ -146,10 +147,20 @@ class AdminHelper:
     def vacuum(self):
         # Mail directory purge
         last_mail_purge = None
+        #last_vacuum = None
+        #list_table = ['being', 'being_mob', 'being_mob_meta', 'being_mob_private', 'being_troll', 'being_troll_private', 'being_troll_private_capa', 'capa_meta', 'champi', 'champi_private', 'coterie', 'event_aa', 'event_battle', 'event_cdm', 'event_champi', 'event_cp', 'event_follower', 'event_tp', 'event_tresor', 'event_user', 'guilde', 'hook', 'lieu', 'lieu_piege', 'lieu_portail', 'maisonnee', 'tresor', 'tresor_meta', 'tresor_private', 'user', 'user_mh_call', 'user_partage']
         while True:
             now = datetime.datetime.now()
             if last_mail_purge is None or (now - last_mail_purge).total_seconds() >= self.mailRetention * 60:
                 users = sg.db.session.query(User).all()
                 for user in users:
                     sg.wk.purge(user, 'archive')
+            # inutile, autovacuum fait le jon
+            #if (last_vacuum is None or (now - last_vacuum).total_hours() >= 10) and now.hour == 9:
+            #    sg.db.session.execute(sql.text("commit"))   # vacuum must run out of a transaction
+            #    for table in list_table:
+            #        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' avant vacuum ' + table)
+            #        sqlu = "vacuum sciz.sciz." + table
+            #        res = sg.db.session.execute(sql.text(sqlu))
+            #    print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' fin des vacuum')
             time.sleep(60)
