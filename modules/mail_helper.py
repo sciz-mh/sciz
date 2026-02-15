c8aad134246aa33d55fe6ed37050b2815bb3134b
Rouletabille <rouletabille@mountyhall.com>
2026-02-15 20:16:31 +0100

    change methode d'envoi des mails

--- modules/mail_helper.py
+++ modules/mail_helper.py
@@ -5,6 +5,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
 import modules.globals as sg
 
 
@@ -14,24 +15,25 @@ class MailHelper:
     # Constructor
     def __init__(self):
         self.load_conf()
-        self.smtp = None
-        try:
-            self.smtp = smtplib.SMTP(self.smtp_host, self.smtp_port, None, 5)
-            self.smtp.ehlo()
-            if self.smtp_tls:
-                self.smtp.starttls()
-                self.smtp.ehlo()
-            # self.smtp.login(self.smtp_from, self.smtp_pwd)
-        except Exception as e:
-            sg.logger.error('Failed to bind to smtp server: %s' % str(e))
+    # smtplib.SMTP version (no more...)
+    #    self.smtp = None
+    #    try:
+    #        self.smtp = smtplib.SMTP(self.smtp_host, self.smtp_port, None, 5)
+    #        self.smtp.ehlo()
+    #        if self.smtp_tls:
+    #            self.smtp.starttls()
+    #            self.smtp.ehlo()
+    #        # self.smtp.login(self.smtp_from, self.smtp_pwd)
+    #    except Exception as e:
+    #        sg.logger.error('Failed to bind to smtp server: %s' % str(e))
 
     # Configuration loader
     def load_conf(self):
-        self.smtp_host = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_HOST]
-        self.smtp_port = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_PORT]
-        self.smtp_tls = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_TLS]
+    #    self.smtp_host = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_HOST]
+    #    self.smtp_port = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_PORT]
+    #    self.smtp_tls = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_TLS]
         self.smtp_from = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_FROM]
-        self.smtp_pwd = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_PWD]
+    #    self.smtp_pwd = sg.conf[sg.CONF_SMTP_SECTION][sg.CONF_SMTP_PWD]
    
     def build_gmail(self):
         if not hasattr(self, 'sender'):
@@ -80,9 +82,10 @@ class MailHelper:
         self.send_mail(None, subject, text, html)
 
     def send_mail(self, to, subject, body_text, body_html):
-        if self.smtp is None:
-            sg.logger.error('An attempt was made to send a mail but no previous bind to a SMTP server was successful')
-            return
+        # smtplib.SMTP version (no more...)
+        #if self.smtp is None:
+        #    sg.logger.error('An attempt was made to send a mail but no previous bind to a SMTP server was successful')
+        #    return
         msg = MIMEMultipart('alternative')
         msg['Subject'] = subject
         msg['From'] = self.smtp_from
@@ -90,10 +93,17 @@ class MailHelper:
         msg.attach(MIMEText(body_text, 'plain'))
         msg.attach(MIMEText(body_html, 'html'))
         if msg['To'] is not None:
-            self.smtp.sendmail(msg['From'], [msg['To']], msg.as_string())
+            # smtplib.SMTP version (no more...)
+            #self.smtp.sendmail(msg['From'], [msg['To']], msg.as_string())
+
+            # sendmail version
+            p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+            p.communicate(msg.as_bytes())
+            sg.logger.info(msg.as_string())
         else:
             sg.logger.warning('No address to send back...')
 
-    def __del__(self):
-        if self.smtp:
-            self.smtp.quit()
+
+    #def __del__(self):
+    #    if self.smtp:
+    #        self.smtp.quit()
